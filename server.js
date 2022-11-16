b39df1dc6c2bd0c91336e43a003f7815c760bca6
sherawat-Lokesh <107927380+sherawat-Lokesh@users.noreply.github.com>
2022-11-16 15:31:30 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const knex = require("knex");
+// const moongose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+//connecting to mongodb database
+// const moongoseUrl =
+//   "mongodb+srv://sherawat-Lokesh:hApddrZuNCu5Qh2@cluster0.c9znsxm.mongodb.net/?retryWrites=true&w=majority";
+
+// moongose
+//   .connect(moongoseUrl, { useNewUrlParser: true })
+//   .then((res) => console.log(res));
+
+//connecting to psql(postgresql) for fetching home page card components
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "12345@Temp",
+    database: "deals4_database",
+  },
+});
+//home page response
+app.get("/", (req, res) => {
+  db.select("*")
+    .from("cardcomponent")
+    .then((data) => {
+      return res.json(data);
+    });
+});
+
+app.listen(8000, () => console.log("server working"));
+
+//article page
+app.post("/article", (req, res) => {
+  try {
+    const { fileName } = req.body;
+    res.sendFile(path.join(__dirname, fileName));
+  } catch (err) {
+    res.json("Something went wrong!");
+  }
+});
