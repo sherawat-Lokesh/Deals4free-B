644245fdbe4e0ae36941e8bb392b12acce5da326
sherawat-Lokesh <107927380+sherawat-Lokesh@users.noreply.github.com>
2022-12-03 22:44:02 +0530

    fixed error of cors

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 
